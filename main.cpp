e09698142947ec3d3a3faed2676404b24c55ab6c
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-21 21:33:26 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,58 @@
+// 1. Debug the code. Take input a character, print 1, if its a capital alphabet, print 0, if its a lowercase alphabet, else print -1.
+
+//Ques 1
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+// 	char c;
+//     cin>>c;
+//     if('a'<=c || c<= 'z'){
+//         cout<<0;
+//     }
+// 	else if('A'<=c || c<= 'Z'){
+//         cout<<1;
+//     }
+//     else{
+//         cout<<-1;
+//     }
+// }
+//Answer 1
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+// 	char c;
+//     cin>>c;
+//     if('a'<=c && c<= 'z'){
+//         cout<<0;
+//     }
+// 	else if('A'<=c && c<= 'Z'){
+//         cout<<1;
+//     }
+//     else{
+//         cout<<-1;
+//     } 
+// }
+
+//Ques 2 :Debug the code. It is trying to print the given pattern.
+// #include<iostream>
+// using namespace std;
+
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j = i,count = 1;
+//         while(count<=i){
+//             cout<<j;
+//             j = j + 1;
+//             count = count + 1;
+//         }
+//         cout<<"\n";
+//         i = i + 1;
+//     }
+// }
+
