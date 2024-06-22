57ca4b1db7f07929b4df382f11524371e4fd8439
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-22 22:09:33 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -55,4 +55,89 @@
 //         i = i + 1;
 //     }
 // }
+//question
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<n){
+//         int j = i,count = 2*n+1,gaps=(n-i-1),k=1;
+//         while(k<gaps){
+//             cout<<" ";
+//             k = k + 1;
+//         }
+//         int m = 1,ch = count - 2*gaps;
+//         while(m<ch){
+//             cout<<"*";
+//             m = m + 1;
+//         }
+//         k = 1;
+//         while(k<gaps){
+//             cout<<" ";
+//             k = k + 1;
+//         }
+//         cout<<"\n";
+//         i = i + 1;
+//     }
+// }
 
+// //Answer
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j = i,count = 2*n-1,gaps=(n-i),k=1;
+//         while(k<=gaps){
+//             cout<<" ";
+//             k = k + 1;
+//         }
+//         int m = 1,ch = count - 2*gaps;
+//         while(m<=ch){
+//             cout<<"*";
+//             m = m + 1;
+//         }
+//         k = 1;
+//         while(k<=gaps){
+//             cout<<" ";
+//             k = k + 1;
+//         }
+//         cout<<"\n";
+//         i = i + 1;
+
+// //question
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int i,j,n;
+//     cin>>n;
+//     for(i=1;i<n;i++){
+//         for(j=1;j<n;j++){
+// 	        cout<<i;
+//         }
+//     cout<<endl;
+//     }
+// }
+
+// //answer
+#include<iostream>
+using namespace std;
+
+int main(){
+  int i,j,n;
+    cin>>n;
+    for(i=1;i<=n;i++){
+
+        for(j=1;j<=i;j++){
+
+        cout<<i;
+        }
+    cout<<endl;
+    }
+}
